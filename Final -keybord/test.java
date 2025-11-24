2f909d60e718060fc3180b28b318e3c26007fb94
Ompatel155 <ompatel7898803421@gmail.com>
2025-11-24 20:38:39 +0530

    new

---
+++ Final -keybord/test.java
@@ -0,0 +1,7 @@
+class test{
+     public static void main(String[] args) {
+         final int x =10;
+         System.out.println(x);
+
+}
+}
---
+++ PACKAGES/src/Main.java
@@ -0,0 +1,7 @@
+import p1.A;
+public class Main{
+    public static void Main(String[] args) {
+        A s1=new A();
+       s1.show();
+    }
+}
