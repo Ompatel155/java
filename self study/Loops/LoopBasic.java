916fbebd6543fcdb22354ed84bf0ca49da7eb694
Ompatel155 <ompatel7898803421@gmail.com>
2025-11-24 22:20:41 +0530

    new

---
+++ self study/Loops/LoopBasic.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class LoopBasic{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.err.print("Enter n : ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println("HELLO.....!");
+        }
+    }
+}
---
+++ self study/Loops/for-Loop/Tableof19.java
@@ -0,0 +1,7 @@
+class Tableof19{
+    public static void main(String[] args) {
+        for(int i=19;i<=10*19;i+=19){
+            System.out.println(i);
+        }
+    }
+}
