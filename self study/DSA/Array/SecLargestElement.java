27001d477841b70d1c7810a97d93bac8ca7dc1b1
Ompatel155 <ompatel7898803421@gmail.com>
2026-01-15 15:36:10 +0530

    new

--- self study/DSA/Array/SecLargestElement.java
+++ self study/DSA/Array/SecLargestElement.java
@@ -3,7 +3,8 @@ class SecLargestElement{
         int arr[]={2,4,6,8,9};
         int max = Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
-          if(arr[i]>max)max=arr[i];
+          if(arr[i]>max)
+          max=arr[i];
         }
         int smax = Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
