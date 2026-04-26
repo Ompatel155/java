5ad8df9d3b16c058afd29ca4482c166f32423e05
Ompatel155 <ompatel7898803421@gmail.com>
2026-04-26 21:41:20 +0530

    new

---
+++ self study/recursion/TenToOne.java
@@ -0,0 +1,11 @@
+class TenToOne{
+    static void printNum(int n) {
+        if (n==0) return;
+        System.out.println(n);
+         printNum(n-1);
+    }
+    public static void main(String[] args) {
+        int n=10;
+        printNum(n);
+    }
+}
---
+++ self study/recursion/factorial.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+class factorial{
+    static int fact(int n){
+        if(n==0) return 1;
+        return n*fact(n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        int n = sc.nextInt();
+        System.out.println("factorial : "+fact(n));
+
+    }
+}
